8ded50a2aeb275ef2f8a43dac670a00d7d384d9a
Your Name <your.email@example.com>
2024-11-14 12:33:24 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,79 @@
+
+let openNavIcon = document.getElementById('open-nav');
+
+let closeNavIcon = document.getElementById('close-nav');
+
+let sideBar = document.getElementById('sidebar');
+
+let ph1 = document.getElementById('ph-1');
+
+let ph2 = document.getElementById('ph-2');
+
+let ph3 = document.getElementById('ph-3');
+
+let div1 = document.getElementById('div-1');
+
+let div2 = document.getElementById('div-2');
+
+let div3 = document.getElementById('div-3');
+
+let image1 = document.getElementById('image-1');
+
+let image2 = document.getElementById('image-2');
+
+function openNav() {
+
+  sideBar.classList.remove('translate-y-full');
+  
+  sideBar.classList.add('translate-y-0');
+  
+  openNavIcon.classList.add('hidden');
+
+  closeNavIcon.classList.remove('hidden');
+
+}
+function closeNav() {
+
+  sideBar.classList.add('translate-y-full');    
+
+  sideBar.classList.remove('translate-y-0');
+  
+  openNavIcon.classList.remove('hidden');
+  
+  closeNavIcon.classList.add('hidden');
+
+}
+function showImage(imageId) {
+                                    
+  ph1.classList.add("opacity-0");
+  
+  ph2.classList.add("opacity-0");
+  
+  ph3.classList.add("opacity-0");
+          
+  document.getElementById(imageId).classList.remove("opacity-0");
+}
+function resetImage() {
+
+  showImage("ph-1");
+
+}
+window.addEventListener("load", function() {
+
+  showImage("ph-1");
+
+});
+function moveRight() {
+
+  image1.classList.toggle('translate-x-[110%]');
+
+  image2.classList.toggle('-translate-x-[100%]');
+
+}
+function moveLeft() {
+
+  image1.classList.toggle('translate-x-[110%]');
+
+  image2.classList.toggle('-translate-x-[100%]');
+
+}
---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html","./assets/**/*.{html,js} ./index.html","./assets/js/**/*.js","./assets/**/*.html"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "1rem",
+      },
+      colors: {
+        'primary': "#009D8C",
+        'secondary': "#374151",
+        'tertiary': "#1F2937",
+        'quaternary': "#6B7280",
+        'senary': "#F3F4F6",
+        'septenary': "#C08D54",
+      },
+      fontFamily: {
+        'primary':["Poppins"],
+        'secondary':[" DM Sans "],
+      },
+      screens: {
+        'xs': '400px',
+        // => @media (min-width: 400px) { ... }
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+        '2md': '850px',
+        // => @media (min-width: 850px) { ... }
+        '3md': '970px',
+        // => @media (min-width: 970px) { ... }
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+        '2lg': '1120px',
+        // => @media (min-width: 1120px) { ... }
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      },
+    },
+  },
+  plugins: [],
+}
+
