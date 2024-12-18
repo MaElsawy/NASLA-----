3030074439647a68c1074f5739966d141e2ffa03
Your Name <your.email@example.com>
2024-12-18 10:38:26 +0200

    scroll-bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -49,6 +49,18 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.no-scrollbar': {
+          '-ms-overflow-style': 'none',
+          'scrollbar-width': 'none',
+        },
+        '.no-scrollbar::-webkit-scrollbar': {
+          display: 'none',
+        },
+      });
+    },
+  ],
 }
 
